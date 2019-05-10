dd1bed7cf637c3c121faf884c96aa72b41b8c4ec
dortega <dortega@tiempodevelopment.com>
2019-05-10 15:16:42 -0500

    git ignore node_modules

--- routing/microservices/reportService.js
+++ routing/microservices/reportService.js
@@ -6,19 +6,19 @@ const serviceURL = require('../serviceRegistry.js');
 const BASE_URL = serviceURL.reportsService();
 const api = apiAdapter(BASE_URL)
 
-router.get('/report/user/', (req, res) => {
+router.get('/reports/users/:userId', (req, res) => {
   api.get(req.path).then(resp => {
     res.send(resp.data)
   })
 })
 
-router.get('/report/admin/tasks', (req, res) => {
+router.get('/reports/admin/tasks', (req, res) => {
   api.get(req.path).then(resp => {
     res.send(resp.data)
   })
 })
 
-router.get('/report/admin/habits', (req, res) => {
+router.get('/reports/admin/habits', (req, res) => {
     api.get(req.path).then(resp => {
       res.send(resp.data)
     })
